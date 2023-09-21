616f30b3bc753865a01e69264824c0f892f51339
hyohyo814 <sojaisam@gmail.com>
2023-09-20 18:38:57 -0700

    checkpoint

---
+++ switch.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main()  {
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a boolean")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
